4a4cbbd69701362cfd4e3df8c352bc1498e6a63f
moviesallwatchdowanload-web <moviesallwatchdowanload@gmail.com>
2026-09-21 04:27:46 +0530

    Build 4586b7cb1c3d929ac6a87fb1c7f04f00ed73b945

---
+++ HdHub4uProvider/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "HDHub4u - Hindi Dubbed Movies & Web Series"
+    authors = listOf("moviesallwatchdowanload-web")
+
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://new6.hdhub4u.cl/favicon.ico"
+    isCrossPlatform = true
+}
